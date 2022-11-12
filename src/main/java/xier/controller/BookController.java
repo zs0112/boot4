c42e2bc0b4c4c7d9edbfc085f29b88ebbca32968
zs0112 <1958929104@qq.com>
2022-11-12 18:04:23 +0800

    first hhot

--- src/main/java/xier/controller/BookController.java
+++ src/main/java/xier/controller/BookController.java
@@ -1,9 +1,11 @@
 package xier.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import xier.service.IBookService;
 
 /**
  * <p>
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/book")
 public class BookController {
-
+@Autowired
+    IBookService bookService;
 }
 
